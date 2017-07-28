3411bcbc9a7c874a4bc4e4baee6d3a6f1dd70c81
Brian Michel <brian.michel@gmail.com>
2017-07-28 15:59:51 -0400

    Initial Commit

---
+++ SpotlightPains/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpotlightPains
+//
+//  Created by Brian Michel on 7/28/17.
+//  Copyright © 2017 Brian Michel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SpotlightPains/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SpotlightPains
+//
+//  Created by Brian Michel on 7/28/17.
+//  Copyright © 2017 Brian Michel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
